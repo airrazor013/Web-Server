9c8ab7ddd2e9e871ed180e93fcc5b9832156bfff
Camden Thomas <camdent@iastate.edu>
2019-09-10 19:00:16 -0500

    updated gitignore

---
+++ web.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+var port = 8080;
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var url = require('url');
+var color = require('./clog.js');
+var clog = color.clog;
+
+
+app.get("/", (req, res) => {
+	sendPage("/index", res);
+});
+app.get("/*", (req, res) => {
+		sendPage(req.path, res);
+});
+
+	
+function sendPage(path, res){
+	clog("Got page request for: " + path , color.yellow);
+	fs.readFile("pages"+path+".html", function(err, page){
+		if(err){
+			sendError(res);
+		}else{
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.end(page);
+			clog('Sent page as response.\n', color.green);
+		}
+	});
+}
+
+function sendError(res){
+	fs.readFile("pages/404.html", function(err, page){
+		res.writeHead(404);
+		res.end(page);
+		clog('Error:\tpage not found.\n\tReturning 404 page.\n', color.red);
+	});
+}
+
+app.listen(port);
+ 
+clog('Server started listening on '+port+' for requests.',color.blue);
+    
